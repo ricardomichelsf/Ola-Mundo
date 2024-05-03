c89ea514dbf36abd693b2335ec3eff94c0b1c8b9
Ricardo Michel Silva <65862245+ricardomichelsf@users.noreply.github.com>
2024-05-03 18:15:55 -0300

    "Added comments and explanation about defining functions in Python"
    This commit adds new comments and explanation about defining functions in Python to the 'funcoes.py' file. It includes information about the 'def' keyword, function name conventions

--- Python - Udemy/funcoes.py
+++ Python - Udemy/funcoes.py
@@ -14,10 +14,25 @@ DRY - Don't Repeat Yourself - Não repeta você mesmo / Não repita seu código
 
 Protocolo senac dia 03/05/2024: 4633384
 
-"""
+
 cores = ['verde', 'amarelo', 'azul', 'branco']
 cores.append('Roxo')
 # Utilizando a função integrada (built-in) do python print()
 print(cores)
 cores.clear()
 print(cores)
+
+Mas então, como definir funções?
+
+Em Python, a forma de definir uma função é:
+def nome_da_funcao(parametros):
+    bloco_da_função
+
+Onde: 
+nome_da_função -> SEMRPRE, com letras minúsculas, e se for nome composto, separado por underline.
+parametros -> Opcionais, onde tendo mais de um, cada um separado por vírgula, podendo ser opcionais ou não.
+bloco_da_função -> Também chamado de corpo da função ou implementação, é onde o processamento da função acontece.
+Neste bloco, pode ter ou não retorno da função.
+
+OBS: Veja que para definir uma função, utilizamos a palavra reservada 'def' informando ao Python que estamos definindo
+"""
