7762996db9f65cfd3b55b1c4800fa5c955e903ba
Ricardo Michel Silva <65862245+ricardomichelsf@users.noreply.github.com>
2024-05-03 18:40:02 -0300

    "Added examples and explanations for functions in Python"
    This commit adds new content to the 'funcoes.py' file, providing examples and explanations for functions in Python. It includes the definition of two functions ('diz\_

--- Python - Udemy/funcoes.py
+++ Python - Udemy/funcoes.py
@@ -34,5 +34,31 @@ parametros -> Opcionais, onde tendo mais de um, cada um separado por vírgula, p
 bloco_da_função -> Também chamado de corpo da função ou implementação, é onde o processamento da função acontece.
 Neste bloco, pode ter ou não retorno da função.
 
-OBS: Veja que para definir uma função, utilizamos a palavra reservada 'def' informando ao Python que estamos definindo
+OBS: Veja que para definir uma função, utilizamos a palavra reservada 'def' informando ao Python que estamos definindo uma função
+Também abrimos o bloco de código com ojá conhecido dois pontos : que é utilizado em Python para definir blocos
+
+Definido a primeira função
+
+def diz_oi():
+    print('Oi!')
+
+Obs: 
+1 - Veja que, dentro das nossas funções podemos utilizar outras funções;
+2 - Veja que essa função não recebe nenhumm paramentro de entrada
+3 - Veja que esta função não retorna nada;
+
+def cantar_parabens():
+    print("Parabéns pra você")
+    print('Nesta data querida')
+    print('Muitas felicidades')
+
+    
+for n in range(5):
+    cantar_parabens()
+
+Em Python, podemos inclusive criar variáveis do tipo de uma funçãoe executar esta função através de variável
+
+canta = canta_parabens
+
+canta()
 """
